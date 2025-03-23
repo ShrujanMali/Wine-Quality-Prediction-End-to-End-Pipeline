37d6abcb1073f5100a45d4ff1777c1e89db3aca4
shrujanmali24@gmail.com <shrujanmali24@gmail.com>
2025-03-24 00:23:35 +0530

    model evaluation and mlflow added

--- src/mlProject/pipeline/stage_01_data_ingestion.py
+++ src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -14,6 +14,7 @@ class DataIngetionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+        
 
 
 if __name__ == "__main__":
